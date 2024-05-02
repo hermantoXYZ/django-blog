4fbe426f0483cabd058eecb41d2a23fc19ccd00f
hermantoXYZ <andiupdates@gmail.com>
2024-05-02 19:47:19 +0700

    update static

--- myblog/settings.py
+++ myblog/settings.py
@@ -16,7 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
